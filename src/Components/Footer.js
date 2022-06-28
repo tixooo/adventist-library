5ba4c2bf230fcd28b8467719181f1f4796696f1c
tixooo <konstantin.stefanov87@gmail.com>
2022-06-29 00:39:59 +0300

    created react app and cleaned it up

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return <>
+        <footer>
+            <p>this is a footer</p>
+        </footer>
+    </>
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return <>
+    <header>
+        <p>this is a header</p>
+    </header>
+    </>
+}
+
+export default Header
